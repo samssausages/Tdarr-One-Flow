f18a874f10ae54ecd4de62170897e041e29f581a
SamSausages <87925618+samssausages@users.noreply.github.com>
2025-03-21 11:54:16 -0500

    Update js_audio_is_lossless.js
    expand list of codecs

--- plugins/js_audio_is_lossless.js
+++ plugins/js_audio_is_lossless.js
@@ -1,7 +1,8 @@
 module.exports = async (args) => {
-  // Define lossless audio codecs
-  const losslessCodecs = ["pcm_s16le", "pcm_s24le", "pcm_f32le"];
   
+  // Define lossless audio codecs
+  const losslessCodecs = ["alac", "dca", "flac", "mlp", "pcm_alaw", "pcm_bluray", "pcm_dvd", "pcm_f32be", "pcm_f32le", "pcm_f64be", "pcm_f64le", "pcm_mulaw", "pcm_s16be", "pcm_s16be_planar", "pcm_s16le", "pcm_s16le_planar", "pcm_s24be", "pcm_s24daud", "pcm_s24le", "pcm_s24le_planar", "pcm_s32be", "pcm_s32le", "pcm_s32le_planar", "pcm_s64be", "pcm_s64le", "pcm_s8", "pcm_s8_planar", "pcm_u16be", "pcm_u16le", "pcm_u24be", "pcm_u24le", "pcm_u32be", "pcm_u32le", "pcm_u8", "pcm_vidc", "sonicls", "tta", "wavpack"];  
+
   // Check if the audio codec of the file is in the lossless list
   const streams = args.inputFileObj.ffProbeData.streams || [];
   let isLossless = false;
